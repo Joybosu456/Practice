35980b7c32b64db4b3659525d73edfba72e8a736
JOY <joybosu@gmail.com>
2022-02-23 20:04:24 +0530

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Practice Problem:-");
+            RvarseProblem.Practice1();
+             PalondromeNum.PalnNumber();
+            RevarseString.RevString();
+            fe.FebonaciSerise();
+            PrefectNum.PerfectNumberFind();
+            Cupon.Cupon_Number();
+            LeapYear.LeapYearCheck();
+            PowerOfTwo.PowerOfTwoCheck();
+            OddEven.OddEvenNum();
+            Fibonacci.Fibonaccinu();
+            Remainder.RemainderNum();
+            SwapNum.SwapTwoNum();
+           Alphabet.AlphabetCheck();
+            FindLargeFindLargestNum();
+
+        }
+    }
+}

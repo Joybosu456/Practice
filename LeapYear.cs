ac925811972752af4df271efd9c72b4ebaaeca3f
JOY <joybosu@gmail.com>
2022-02-23 20:21:35 +0530

    Check the year is leap year or not

---
+++ LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class LeapYear
+    {
+        public static void LeapYearCheck()
+        {
+            Console.WriteLine("Enter The year :- ");
+            int year = int.Parse(Console.ReadLine());
+            {
+                if ((year % 4 == 0) && (year % 100 !== 0 || year % 400 == 0)))
+                  {
+                    Console.WriteLine(year + " is a leap year ");
+                  }
+                else
+                {
+                    Console.WriteLine(year + " is not a leap year");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}

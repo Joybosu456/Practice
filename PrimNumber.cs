35d2b332d1959c86cf2b5c226fbf8f773b80f0a1
JOY <joybosu@gmail.com>
2022-02-23 20:14:28 +0530

    Find Prime Number

---
+++ PrimNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class PrimNumber
+    {
+        public static void PrimeNUmCheck()
+        {
+            Console.WriteLine("Please enter number :");
+            int number=Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 1)
+            {
+                Console.WriteLine("invalid number");
+            }
+            else
+            {
+                int flag = 0;
+                for (int i = 2; i < number; i++)
+                {
+                    if (number / i == 0)
+                    
+                        Console.WriteLine({ 0} "is not prime number");
+                   flag = 1;
+                   break;
+            }
+            
+                
+            
+        }
+    }
+}

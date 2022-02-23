bc8f78d788d9c7f4ba497b862d815c4530a6fb02
JOY <joybosu@gmail.com>
2022-02-23 20:17:40 +0530

    Power of two program upload

---
+++ PowerOfTwo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class PowerOfTwo
+    {
+        public static void PowerOfTwoCheck()
+        {
+            int num, power;
+            Console.WriteLine("Enter the number :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number1 :");
+            power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Answer is : " + pow(num, power));
+            Console.ReadLine();
+
+        }
+        //Recurcive Method
+        public static int pow(int num, int power)
+        {
+            if (power == 0)
+                return 1;
+            return num * pow(num, --power);
+        }
+        
+    }
+    
+}

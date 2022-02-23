a09a8ae383962eaf1db911ce282f736464bd2d6d
JOY <joybosu@gmail.com>
2022-02-23 20:09:51 +0530

    upload RvarseProblem program

---
+++ RvarseProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class RvarseProblem
+    {
+        public static void Practice1()
+        {
+            Console.WriteLine("Plese enter your Input:=");
+            int input=Convert.ToInt32(Console.ReadLine());
+
+            int reminder, revarse = 0;
+            while(input>0)
+            {
+                reminder = input % 10;
+                revarse = revarse * 10 + reminder;
+                input = input / 10;
+            }
+            Console.WriteLine("revarse number is" +revarse);
+        }
+    }
+}

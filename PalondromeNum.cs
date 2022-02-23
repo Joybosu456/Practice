bfadc8485d749d5fb869942ac13228391d04d125
JOY <joybosu@gmail.com>
2022-02-23 20:19:44 +0530

    upload palondrom Number program

---
+++ PalondromeNum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class PalondromeNum
+    {
+        public static void PalnNumber()
+        {
+            Console.WriteLine("Please provide your Input number:=");
+            int input=Convert.ToInt32(Console.ReadLine());
+
+            int backup = input;
+            int remainder, revarse = 0;
+
+            while(input > 0)
+            {
+                remainder = input % 10;
+                revarse = revarse * 10 + remainder;
+                input = input / 10;
+            }
+            if(backup==revarse)
+            {
+                Console.WriteLine(backup + "is a palondrom number");
+            }
+            else
+            {
+                Console.WriteLine(backup + "is not a palondrom number");
+            }
+        }
+    }
+}
